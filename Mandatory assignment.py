bd772320bd70aefcd22ca974f154089a73f46aad
andreas-eklundh <andreas.eklundh@hotmail.com>
2025-03-11 17:29:06 +0100

    Initial commit of utils and initial testings

---
+++ Mandatory assignment.py
@@ -0,0 +1,311 @@
+### Mandatory assignment
+# Various utils. 
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm ,binom, t, expon, invgamma
+from scipy.optimize import minimize 
+from scipy.stats import multivariate_normal as mnorm
+from scipy.stats import multivariate_t as mt
+import pandas as pd
+# Utils
+import utils as u
+
+# read data and initial prep. 
+data = pd.read_csv(f'Data/stock_data.csv').sort_values('Date')
+N_obs = len(data['Date'])
+# Get log returns. 
+name_list = ['GOOG','MSFT','MRK','IDU']
+for s in ['GOOG','MSFT','MRK','IDU']:
+    data[s+'_shift'] = data[s].shift(1) # Shift once.
+    data['X_'+s] = np.log(data[s]) - np.log(data[s+'_shift'])
+    data['X_'+s] = data['X_'+s].fillna(0) # Zero return othjerwise.
+
+# convert to arrays
+goog,msft = np.array(data['X_GOOG']),np.array(data['X_MSFT'])
+mrk,idu = np.array(data['X_MRK']),np.array(data['X_IDU'])
+# Get negative returns
+goog_neg,msft_neg,mrk_neg,idu_neg = goog*(-1),msft*(-1), mrk*(-1),idu*(-1)
+
+
+
+### 1. Marginal distributions for each stock.
+u.marginal_plots(goog_neg,df1=2,df2=3,df3=4,df4=5,df5=6,markersize=5)
+u.marginal_plots(msft_neg,df1=2,df2=3,df3=4,df4=5,df5=6,markersize=5)
+u.marginal_plots(mrk_neg,df1=2,df2=3,df3=4,df4=5,df5=6,markersize=5)
+u.marginal_plots(idu_neg,df1=2,df2=3,df3=4,df4=5,df5=6,markersize=5)
+
+# Google seems adequately fit by a t-distr. with df=6 or df=4
+# Msft df =4 is reasonable.
+# Merck df=3 seems reasonable.
+# IDU df=2 seems adequate (maybe df=3 if we want second moment)
+dflist = [5,3,3,3]
+
+### 2. upper tail distributions of losses using EVT. 
+# Note this is only meaningfull for positive values of X
+# i.e. the 'loss tail'
+
+for X,name,df in zip([goog_neg,msft_neg,mrk_neg,idu_neg],name_list,dflist):
+    x = X[X>0]
+    n_obs = x.shape[0]
+    ## Analysing thorugh Hill-Estimator (discard first few obs.):
+    k_grid = np.array([i for i in range(5,200)])
+    alpha_hat = []
+    for k in k_grid:
+        alpha_hat.append(u.HillEstimator(x,k))
+    alpha_hat_50 = np.array(alpha_hat)
+
+    plt.plot(k_grid-1,alpha_hat_50,color = 'black',label='Hill-plot')
+    plt.hlines(y = df,xmin=np.min(k_grid),xmax = np.max(k_grid),color = 'red',label="Index")
+    plt.grid()
+    plt.xlabel('k')
+    plt.ylabel('Alpha estimate')
+    plt.title(f'Hill Plot, {name}')
+    plt.legend()
+    plt.show()
+
+# Evidently from the plot, is that the indices obtained from the 
+# QQ plot analysis are not suffucient
+# google MRk idu somewhat easy to read. Difficult msft
+H_k_goog,H_k_msft,H_k_mrk,H_k_idu = 105,160,130,100
+# Note hor these k's, we may have values 
+a_goog,a_msft = u.HillEstimator(goog_neg,H_k_goog), u.HillEstimator(msft_neg,H_k_msft)
+a_mrk,a_idu = u.HillEstimator(mrk_neg,H_k_mrk),u.HillEstimator(idu_neg,H_k_idu)
+print(f"Indixes: google:{a_goog},Microsoft:{a_msft}")
+print(f"MRK:{a_mrk},IDU:{a_idu}")
+hill_list = [a_goog,a_msft,a_mrk,a_idu ]
+# Coult then be a t-distr with these dfs, or slowly varying
+# With these indices. 
+# This methodology indicates that the 
+# choices from previously was not too much off. 
+
+## POT-Methodolody.
+for X,name in zip([goog_neg,msft_neg,mrk_neg,idu_neg],name_list):
+    x_sorted = - np.sort(-X) # Sort decending. 
+    n_obs = x_sorted.shape[0]
+    k_list = [i for i in range(0,n_obs)]
+    e_u = np.zeros(shape=n_obs)
+    for k in k_list:
+        e_u[k] = u.emp_mean_exess(x_sorted,k)
+    plt.scatter(x_sorted[1:],e_u[1:],label='Mean-Exess plot', 
+                color='green',s=1)
+    plt.grid()
+    plt.xlabel('x_k,n')
+    plt.ylabel("Empirical-mean exess function")
+    plt.title(f'Mean-Exess plotm {name}')
+    plt.show()
+
+# OBS: the X's and k's do not allign yet -> Do negative sort.
+# Get k's - difficult for google nad microsoft.. 
+
+u_goog,u_msft,u_MRK, u_IDU=0.025, 0.04, 0.015, 0.02
+u_list = [u_goog,u_msft,u_MRK, u_IDU]
+pot_indices,beta_list,gamma_list = [],[],[]
+# Do remainder of analysis. 
+for X,thres,name in zip([goog_neg,msft_neg,mrk_neg,idu_neg],u_list,name_list):
+    x_sorted = - np.sort(-X) # Sort decending. 
+    k_idx = np.argmin(x_sorted>thres) - 1 
+    # -1 as idx is where it goes below
+    k = k_idx + 1 # plus 1 as e.g. idx 3 corresponds to 4.
+    print(f"k is {k} (in idx terms), val of k {x_sorted[k_idx]}")
+    # So k = 50 is okay i guess. 
+    # Get exesses axcross 
+    n_obs = x_sorted.shape[0]
+    # tnumber exeeding threshold and retransform. 
+    x_k =  x_sorted[k_idx]
+    Z_i = (x_sorted[:k_idx] -x_k) # exess vals.
+    N_u = len(Z_i)
+    # Optimize numercally. 
+    x0 = [2.6,0.7] # take params from similar problem
+    res = minimize(fun=u.GPD_neg_loglike,x0=x0,args=(N_u,Z_i),
+                method='Nelder-Mead',options={'fatol': 1e-20})
+    beta_hat,gamma_hat = res.x
+    beta_list.append(beta_hat)
+    gamma_list.append(gamma_hat)
+    print(f'Name {name}: Gamma_hat: {gamma_hat}, beta_hat: {beta_hat}')
+    # Not entirely the same as Rasmus, but sufficiently close.
+
+    Z_orig_order = np.array(X[X>x_k] - x_k)
+    G = u.GPD_fun(gamma_hat,beta_hat,Z_orig_order)
+    gen_res = - np.log(G)
+    gen_res_smooth = np.cumsum(gen_res) / np.cumsum(np.ones(N_u))
+    # Sort residuals and Z
+    Z_sorted = np.sort(Z_i)
+    ges_res_sort = np.sort(gen_res)
+    p_vec_tail =  np.array([(i)/(k) for i in range(1,k_idx+1)])
+    q_expo = expon.ppf(p_vec_tail)
+
+    F_n_bar = N_u / n_obs
+    F_n_u_bar = 1 - u.GPD_fun(gamma_hat,beta_hat,Z_sorted)
+    F_bar_upx = F_n_bar * F_n_u_bar
+
+    # plot vs. Pareto counterpart. 
+    F_bar_emp = 1 - np.cumsum(1/n_obs*np.ones(x_sorted.shape[0]))
+    F_bar_emp_tail = F_bar_emp[n_obs-k_idx:]
+
+    # plotting.
+    fig, ax = plt.subplots(nrows=2,ncols=2)
+    # Normal distributions
+    ax1 =  ax[0,0]
+    ax1.scatter([(i) for i in range(0,k_idx)],gen_res, 
+                color='blue',s=1)
+    ax1.plot([(i) for i in range(0,k_idx)],
+             gen_res_smooth,color='black')
+    ax1.grid()
+    ax1.set_xlabel('Ordering (1:k)')
+    ax1.set_ylabel('Generalized residuals')
+    ax1.set_title(f'Residual plot')
+    # t-distributions. 
+    ax2 =  ax[0,1]
+    ax2.scatter(q_expo,ges_res_sort, color='blue',s=1)
+    ax2.plot(q_expo,q_expo,color='black',label='Reference line if distr true')
+    ax2.grid()
+    ax2.set_xlabel('Theoretical quantiles')
+    ax2.set_ylabel('Empirical quantiles')
+    ax2.set_title(f'Exponential quantiles')
+    ax3 =  ax[1,0]
+    ax3.plot((Z_sorted + x_k), F_bar_emp_tail, label='Empirical tail',color='red')
+    ax3.plot((Z_sorted + x_k), F_bar_upx, label='Pareto tail',color='blue')
+    ax3.grid()
+    ax3.set_xlabel('Log returns (x)')
+    ax3.legend()
+    fig.tight_layout()
+    plt.show()
+    pot_indices.append(1/gamma_hat)
+
+# Seems like a fairly adequate fit. 
+# The approximation looks good. The mean of residuals
+# is around 1 (running mean - > find another smoother)
+# Also, the QQ plot of generalied residuals lies on a 
+# straight line indicating that the pareto model is 
+# a reasonable choice. 
+# Finally, compare all indeixes: 
+print(f'Google t-index: {dflist[0]}, Hill: {hill_list[0]}, POT: {pot_indices[0]}')
+print(f'Microsoft t-index: {dflist[1]}, Hill: {hill_list[1]}, POT: {pot_indices[1]}')
+print(f'Merck t-index: {dflist[2]}, Hill: {hill_list[2]}, POT: {pot_indices[2]}')
+print(f'IDU t-index: {dflist[3]}, Hill: {hill_list[3]}, POT: {pot_indices[3]}')
+
+# Really adequate for Merck and IDU to 
+# use this t-distribution or use it with the 
+# EVT obtained indexes. Not as clear for mircrosoft and 
+# google -> Hill plot fairly good, but POT 
+# Does not give good results. 
+# Given the indices, the choises in 1 was reasonable.
+
+
+### 3. Beginning on joint comparisons. 
+X_pf1 = np.array([goog,msft ])
+X_pf2 = np.array([mrk,idu  ])
+X_pf1_neg = np.array([goog_neg,msft_neg ])
+X_pf2_neg = np.array([mrk_neg,idu_neg  ])
+# Do marginal plots: 
+fig, ax = plt.subplots(nrows=1,ncols=2)
+ax1 =  ax[0]
+ax1.scatter(goog_neg,  msft_neg, color='blue',s=1)
+ax1.grid()
+ax1.set_xlabel('Google')
+ax1.set_ylabel('Microsoft')
+ax1.set_title('Log returns of Google and Microsoft')
+ax2 =  ax[1]
+ax2.scatter(mrk_neg,idu_neg, color='blue',s=1)
+ax2.grid()
+ax2.set_xlabel('Merck')
+ax2.set_ylabel('IDU')
+ax2.set_title(f'Log returns of Merck and IDU')
+fig.tight_layout()
+plt.show()
+
+# Propose eliptical distributions for the pair.
+# Not totally functioning. 
+# T-distributions could make sense in either case
+d = X_pf1_neg.T.shape[1]
+nu_init,mu_init,sigma_init  = 4, np.mean(X_pf1_neg.T, axis=0),np.cov(X_pf1_neg)
+A_init = np.linalg.cholesky(sigma_init)
+params_init = np.hstack(([nu_init], mu_init, A_init.flatten()))
+# Optimize the negative log-likelihood
+result = minimize(u.multivariate_t_log_likelihood, params_init, args=(X_pf1_neg.T), method='L-BFGS-B')
+# Extract estimated parameters
+nu_pf1 = result.x[0]
+mu_pf1 = result.x[1:d+1]
+A_pf1 = np.reshape(result.x[d+1:], (d, d))
+sigma_pf1 = A_pf1 @ A_pf1.T
+
+result = minimize(u.multivariate_t_log_likelihood, params_init, args=(X_pf2_neg.T), method='L-BFGS-B')
+# Extract estimated parameters
+nu_pf2 = result.x[0]
+mu_pf2 = result.x[1:d+1]
+A_pf2= np.reshape(result.x[d+1:], (d, d))
+sigma_pf2 = A_pf2 @ A_pf2.T
+# Fit done - Don't know how to fit more
+
+### 4. Copula approach.
+# For marginals, combine to get distribution fcts. 
+# These could be used in analysis below. 
+
+# Over u, use Pareto Tail.
+i_list = [0,1,2,3]
+for X,thres,name,i in zip([goog_neg,msft_neg,mrk_neg,idu_neg],u_list,name_list,i_list):
+    x_sorted = - np.sort(-X) # Sort decending. 
+    beta_est = beta_list[i]
+    gamma_est = beta_list[i]
+    # Get exesses axcross 
+    F_marginal = u.GDP_emp(np.sort(x_sorted),x_k,beta_est,gamma_est)
+
+    plt.plot(np.sort(x_sorted),F_marginal, color='red')
+    plt.xlabel("Log returns")
+    plt.ylabel("Distribution function")
+    plt.grid()
+    plt.title(f"Marginal distribution {name}")
+    plt.show()
+
+
+# We should in principle be able to perform 
+# Simulations for these marginals. 
+
+### 5. Exploratory analysis, upper tail dependence
+# Probably to be done for negative returns 
+rho_tau_pf1 = u.kendalls_tau(X_pf1_neg.T)
+rho_tau_pf2 = u.kendalls_tau(X_pf2_neg.T)
+print(f'rho tau pf1 {rho_tau_pf1}')
+print(f'rho tau pf2 {rho_tau_pf2}')
+# Based on kendall's tau, it seems that especially 
+# Microsoft and Google are comonotone. 
+# But in general somewhat comonotone. 
+
+# Upper tail dependence? 
+
+# Propose Copula: 
+# Use t-copula or Gaussian (does not seem to have one tail 
+# dependence over another -> t-dist). 
+
+# Assuming this, we can use Theorem 11.7 to determine the
+# standard correlation .
+N_sim = 10**6
+rho_gauss1 = np.sin(rho_tau_pf1*np.pi/2)
+rho_gauss2 = np.sin(rho_tau_pf2*np.pi/2)
+
+copulas = u.Copulas()
+rho_mat_1 = np.array([[1,rho_gauss1],
+                      [rho_gauss1,1]])
+U_gauss1 = copulas.simul_Gaussian(rho_mat_1,N_sim)
+
+# Convert to returns:
+X_gauss1 = U_gauss1.copy()
+X_gauss1[:,0] = u.inverse_GDP_emp(U_gauss1[:,0],np.sort(goog_neg),
+                                  u_list[0],beta_list[0],gamma_list[0])
+X_gauss1[:,1] = u.inverse_GDP_emp(U_gauss1[:,1],np.sort(msft_neg),
+                                  u_list[1],beta_list[1],gamma_list[1])
+
+plt.scatter(X_gauss1[:,0],X_gauss1[:,1],color='red')
+plt.grid()
+plt.show()
+
+rho_mat_2 = np.array([[1,rho_gauss2],
+                      [rho_gauss2,1]])
+
+U_gauss2 = copulas.simul_Gaussian(rho_mat_2,N_sim)
+
+### 6. Frechet Bounds
+
+
+### 7. Calculate VaR using varous approaches.
+S_0 = 10000
---
+++ utils.py
@@ -0,0 +1,341 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm ,binom, uniform, t, expon, invgamma,gamma,levy_stable
+from scipy.optimize import minimize
+from scipy.stats import multivariate_normal as mnorm
+from scipy.stats import multivariate_t as mt
+from scipy.special import factorial
+
+def generel_inverse(L,F_L,alpha): 
+    L_ordered = np.sort(L)
+    # See definition in ex. 3.2.
+    F_L_ualpha = np.where(F_L>=alpha,F_L,1)
+    idx = np.argmin(F_L_ualpha) 
+    # Will also find lowest in case of ties by default
+    # Note, due to zero indexing, subtract one. 
+    q_alpha = L_ordered[idx]
+
+    return q_alpha
+
+def VaR(L,alpha):
+    L_order_desc = - np.sort(-L)
+    var_idx=int(np.floor(len(L)*(1-alpha)))
+    VaR = L_order_desc[var_idx]
+
+    return VaR
+
+def ES(L,alpha):
+    L_order_desc = - np.sort(-L)
+    var_idx = int(np.floor(len(L)*(1-alpha)))
+    ES = np.sum(L_order_desc[:var_idx+1]) / (var_idx+1)
+
+    return ES
+
+### EXTREME VALUE THEORY
+
+def HillEstimator(X,k):
+    X_sorted = - np.sort(-X)
+    k_0index = k - 1
+    X_k = X_sorted[k_0index]
+    
+    mean =  np.sum(np.log(X_sorted[:k]) - np.log(X_k)) / k
+    alpha = mean**(-1)
+    return alpha
+
+def emp_mean_exess(X,k):
+    x_k = X[k] # get k'th entry - zero at first.
+    return  np.sum(X[:k+1]-x_k) / (k+1) # due to zero idx.
+
+def GPD_fun(gamma,beta,x):
+    return 1- (1+gamma*x/beta)**(-1/gamma)
+    
+def GPD_neg_loglike(params,N_u,Z):
+    beta,gamma = params
+    log_like = -N_u * np.log(beta) - (gamma + 1) * np.sum(
+        np.log(1+gamma*Z/beta))/gamma
+    return - log_like
+
+# Mandatory assignment -> Create fct combining emp. distr
+# and Pareto.
+def GDP_emp(X,u,beta,gamma):
+    n_obs = X.shape[0]
+    N_u = np.sum(X > u)
+    F_margin = np.zeros(n_obs)
+    for i in range(0,n_obs):
+        # Empirical if below.
+        if X[i] <= u:
+            F_margin[i] = np.sum(X[:i+1] <= u) /  n_obs
+        else: 
+            F_n_u_bar = 1 - GPD_fun(gamma,beta,X[i])
+            F_n_bar = N_u / n_obs
+            F_bar_upx = F_n_bar * F_n_u_bar
+            F_bar_upx_upp = 1 - F_bar_upx
+            F_margin[i] = F_bar_upx_upp
+
+    return F_margin
+
+def inverse_GDP_emp(U,X_emp,u,beta,gamma):
+    x_sorted = - np.sort(-X_emp) # Sort decending. 
+    k_idx = np.argmin(x_sorted>u) - 1 
+    F_margin = GDP_emp(X_emp,u,beta,gamma)
+    F_margin_sort = - np.sort(-F_margin) 
+    out = np.zeros(U.shape[0])
+    for i in range(out.shape[0]):
+    # If slightly above empirical section -> Inverse GDP
+        if U[i] > F_margin_sort[k_idx]:
+            out[i] = beta*((1-U[i])**(-gamma)-1)/gamma
+        else:
+            out[i] = generel_inverse(X_emp,F_margin,U[i])
+    return out
+
+# Alternative approach for this CI section dessirable. 
+def CI_lossRV(p,n,beta):
+    idx_list = np.array([i for i in range(1,n)])
+    P_Ygeqj = 1-binom.cdf(idx_list,n,p)
+    j = np.argmax(np.where(P_Ygeqj<=beta/2,P_Ygeqj,0))
+    P_Yleqj = binom.cdf(idx_list,n,p)
+    i = np.argmax(np.where(P_Yleqj<=beta/2,P_Yleqj,0))
+    return (j,i)
+
+# Regarding Importance sampling 
+def Emp_distr_tail_expo_shift(u,L,X,N,C,xi):
+    F_L = np.sum(1/(C*np.exp(X @ xi)) * (L > u)) / N   
+    return F_L
+
+def Emp_tail_diff(u,L,X,N,C,xi,alpha):
+    return np.abs(Emp_distr_tail_expo_shift(u,L,X,N,C,xi) - alpha)
+
+def emp_tail_solve(L,X,N,C,xi, alpha):
+    # Use relative difference to obtain tmore stable optimization.
+    iv_num = minimize(fun=Emp_tail_diff,
+                      x0=L[0],args = (L,X,N,C,xi, alpha),
+                        method='Nelder-Mead')
+    out = iv_num.x
+    return out
+
+
+
+### MULTIVARIATE T LOG LIKELIHOOD 
+# TODO: REWRITE THIS.
+
+def multivariate_t_log_likelihood(params, data):
+    """
+    Compute the negative log-likelihood of a multivariate t-distribution.
+    """
+    d = data.shape[1]  # Number of dimensions
+    nu = params[0]  # Degrees of freedom
+    mu = params[1:d+1]  # Location vector
+    
+    D = np.reshape(params[d+1:], (d, d)) # Cholesky decomposition of covariance
+
+    sigma = D @ D.T  # Reconstruct covariance matrix
+    dist = mt(loc=mu, shape=sigma, df=nu)
+    
+    return -np.sum(dist.logpdf(data))
+
+
+### FOR INVESTIGATING COMONOCITY AND COUNTERMONOCITY.
+
+def kendalls_tau(X):
+    '''
+    Takes X \in R^(n,2) i.e. assumes bivariate R.V
+    '''
+    n = X.shape[0]
+    k = 2 # bivariate comparisons. 
+    binom_coeff = factorial(n,exact=True) / (
+        factorial(k,exact=True)*factorial(n-k,exact=True))
+    sign_sum = 0
+    for i in range(0,n):
+        sign_sum += np.sum(np.sign((X[i,0]-X[i:,0])*(X[i,1]-X[i:,1]))) 
+    return sign_sum / binom_coeff
+
+# Merge sort for efficient kendalls tau. 
+def merge_sort_count(arr):
+    """
+    Modified merge sort to count the number of inversions (discordant pairs).
+    """
+    if len(arr) <= 1:
+        return arr, 0
+    
+    mid = len(arr) // 2
+    left, left_inv = merge_sort_count(arr[:mid])
+    right, right_inv = merge_sort_count(arr[mid:])
+    
+    merged = []
+    i = j = inv_count = 0
+    
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+            inv_count += len(left) - i
+    
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+    
+    return merged, left_inv + right_inv + inv_count
+
+def kendalls_tau_eff(X):
+    """
+    Compute Kendall's Tau correlation coefficient efficiently using Merge Sort.
+    """
+    x,y = X[:,0], X[:,1]
+    assert len(x) == len(y), "Both lists must have the same length."
+    
+    n = len(x)
+    pairs = list(zip(x, y))
+    pairs.sort()
+    
+    sorted_y = [val for _, val in pairs]
+    _, discordant = merge_sort_count(sorted_y)
+    
+    total_pairs = (n * (n - 1)) // 2
+    concordant = total_pairs - discordant
+    
+    tau = (concordant - discordant) / total_pairs if total_pairs > 0 else 0
+    return tau
+
+### Copula class containing functionality to simulate from 
+
+class Copulas():
+    def __init__(self):        
+        return None
+    
+    # Create Various copula functions as fct of params
+
+    # Gumbel Copula.
+    def C_gumbel(self,theta,U):
+        return np.exp(-(np.sum((-np.log(U))**theta,axis=1))**(1/theta))
+
+    def simul_gumbel(self,theta,dim,N_sim):
+        # Step 1: Identify the distr. G having Laplace Transform 
+        # psi = theta^{-1}
+        # Step 2: Simulate V~G
+        gamma = (np.cos(np.pi/(2*theta)))**theta
+        V = levy_stable.rvs(alpha=1/theta,beta=1,scale=gamma,
+                            size=N_sim).reshape((N_sim,1))
+        # Step 3: Generate U_1,...,U_d
+        U = uniform.rvs(size=dim*N_sim).reshape((N_sim,dim))
+        # Get W
+        t = - np.log(U) / V
+        W = np.exp(-t**(1/theta))
+        # Copula value
+        #C_gumbel = self.C_gumbel(theta,U)
+
+        return W #,C_gumbel 
+
+    # Clayton Copula - > Make general version.
+    def C_clayton(self,theta,U,dim):
+        return (np.sum(U**(-theta),axis=1)-dim+1)**(-1/theta)
+
+    # We also need to simulate from these. 
+    # Note this follows Example 10.6 and prop 10.8
+    def simul_clayton(self,theta,dim,N_sim):
+        # Step 1: Identify the distr. G having Laplace Transform 
+        # psi = theta^{-1}
+        # Here G is Gamma w. alpha=beta=1/theta
+        # See the documentation
+        # Step 2: Simulate V~G
+        V = gamma.rvs(a=1/theta,scale=1/theta,size=N_sim).reshape(
+            (N_sim,1))
+        # Step 3: Generate U_1,...,U_d
+        U = uniform.rvs(size=dim*N_sim).reshape((N_sim,dim))
+        # Get W
+        t = - np.log(U) / V
+        W = (theta * t + 1)**(-1/theta)
+        # Copula value
+        #C_clayton = self.C_clayton(theta,U,dim)
+
+        return W #,C_clayton
+
+    def simul_Gaussian(self,rho_mat,N_sim):
+        d = rho_mat[0,:].shape[0]
+        mu = np.zeros(d)
+        X = mnorm.rvs(mean=mu,cov=rho_mat,size=N_sim)
+        U = norm.cdf(X,0,1)
+        cop_in = norm.ppf(U)
+        #C_gauss = mnorm.cdf(cop_in,mean=mu,cov=rho_mat)
+        # Return both obs and fct val. 
+        return U #,C_gauss)
+    
+    def simul_t_distr(self,rho_mat,nu,N_sim):
+        d = rho_mat[0,:].shape[0]
+        mu = np.zeros(d)
+        X = mt.rvs(loc=mu,shape=rho_mat,df=nu,size=N_sim)
+        U = t.cdf(X,df=nu)
+        # Then we need to tranform U in principle (will be x)
+        cop_in = t.ppf(U,df = nu)
+        #C_nu_sigma = mt.cdf(cop_in,loc=mu,shape=rho_mat,df=nu)
+        # Return both obs and fct val. 
+        return U #(U,C_nu_sigma)
+    
+
+    
+### Marginal distribution plots -> 1 Gaussian and 3 t-distributions 
+def marginal_plots(X,df1,df2,df3,df4,df5,markersize):
+    X_sorted = np.sort(X)
+    #X_mean = np.mean(X_sorted)
+    #X_std = np.std(X_sorted)
+    n = X_sorted.shape[0]
+    p_vec = np.array([(i)/(n+1) for i in range(1,n+1)])
+    # NOTE: I DEVIATE FROM THE NOTES, BUT QQ-PLOTS GO FROM SMALLEST TO LARGETST. 
+    # Do 4 plots. 1 for normal and three t-distr. 
+    q_norm = norm.ppf(p_vec ) #,loc=X_mean,scale=X_std)
+    q_t1 = t.ppf(p_vec,df=df1)#,loc=X_mean,scale=X_std)
+    q_t2 = t.ppf(p_vec,df=df2)#,loc=X_mean,scale=X_std)
+    q_t3= t.ppf(p_vec,df=df3)#,loc=X_mean,scale=X_std)
+    q_t4= t.ppf(p_vec,df=df4)#,loc=X_mean,scale=X_std)
+    q_t5= t.ppf(p_vec,df=df5)#,loc=X_mean,scale=X_std)
+
+
+    # Plotting: 
+    fig, ax = plt.subplots(nrows=3,ncols=2)
+    # Normal distributions
+    ax1 =  ax[0,0]
+    ax1.scatter(q_norm,X_sorted, color='blue',s=markersize)
+    #ax1.plot(q_norm,q_norm,color='black',label='Reference line if distr true')
+    ax1.grid()
+    ax1.set_xlabel('Theoretical quantiles')
+    ax1.set_ylabel('Empirical quantiles')
+    ax1.set_title(f'Normal distribution')
+    # t-distributions. 
+    ax2 =  ax[0,1]
+    ax2.scatter(q_t1,X_sorted, color='blue',s=markersize)
+    #ax2.plot(q_t1,q_t1,color='black',label='Reference line if distr true')
+    ax2.grid()
+    ax2.set_xlabel('Theoretical quantiles')
+    ax2.set_ylabel('Empirical quantiles')
+    ax2.set_title(f'T-distribution, df={df1}')
+    ax3 =  ax[1,0]
+    ax3.scatter(q_t2,X_sorted, color='blue',s=markersize)
+    #ax3.plot(q_t2,q_t2,color='black',label='Reference line if distr true')
+    ax3.grid()
+    ax3.set_xlabel('Theoretical quantiles')
+    ax3.set_ylabel('Empirical quantiles')
+    ax3.set_title(f'T-distribution, df={df2}')
+    ax4 =  ax[1,1]
+    ax4.scatter(q_t3,X_sorted, color='blue',s=markersize)
+    #ax4.plot(q_t3,q_t3,color='black',label='Reference line if distr true')
+    ax4.grid()
+    ax4.set_xlabel('Theoretical quantiles')
+    ax4.set_ylabel('Empirical quantiles')
+    ax4.set_title(f'T-distribution, df={df3}')
+    ax5 =  ax[2,0]
+    ax5.scatter(q_t4,X_sorted, color='blue',s=markersize)
+    #ax5.plot(q_t4,q_t4,color='black',label='Reference line if distr true')
+    ax5.grid()
+    ax5.set_xlabel('Theoretical quantiles')
+    ax5.set_ylabel('Empirical quantiles')
+    ax5.set_title(f'T-distribution, df={df4}')
+    ax6 =  ax[2,1]
+    ax6.scatter(q_t5,X_sorted, color='blue',s=markersize)
+    #ax6.plot(q_t5,q_t5,color='black',label='Reference line if distr true')
+    ax6.grid()
+    ax6.set_xlabel('Theoretical quantiles')
+    ax6.set_ylabel('Empirical quantiles')
+    ax6.set_title(f'T-distribution, df={df5}')
+    fig.tight_layout()
+    plt.show()
